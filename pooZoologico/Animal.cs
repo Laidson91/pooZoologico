3854d9cc47aaaadd56d77081d4bbd961a5969ae0
Laidson91 <Laidson_souza@hotmail.com.br>
2023-03-21 15:10:46 -0300

    Adicionar arquivos de projeto.

---
+++ pooZoologico/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooZoologico
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            this.Especie = "";
+            this.Peso = 0;
+        }
+        public Animal(String especie, double peso)
+        {
+            this.Especie = especie;
+            this.Peso = peso;
+        }
+        private String especie;
+
+        public String Especie
+        {
+            get { return especie; }
+            set { especie = value.ToUpper(); }
+        }
+
+        private double peso;
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+
+    }
+}
---
+++ pooZoologico/AnimalPlus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooZoologico
+{
+    public class AnimalPlus:Animal
+    {
+        public AnimalPlus() : base()
+        {
+            this.tipoAlimentacao = "";
+        }
+
+        public AnimalPlus(string especie, double peso, string tipoAlimentacao) : base(especie, peso)
+        {
+            this.TipoAlimentacao = tipoAlimentacao;
+        }
+        private string tipoAlimentacao;
+
+        public string TipoAlimentacao
+        {
+            get { return tipoAlimentacao; }
+            set { tipoAlimentacao = value; }
+        }
+
+    }
+}
